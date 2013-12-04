e78c28ced434f2e72953554c3f00ce6852ad1b98
Krist Wongsuphasawat <kwongsuphasawat@twitter.com>
2013-12-03 16:35:16 -0800

    move templateurl up

--- scripts/directives/chartForm.js
+++ scripts/directives/chartForm.js
@@ -8,13 +8,13 @@ function(directives){
     return {
       restrict: 'E',
       replace: true,
+      templateUrl: 'partial/chartForm.html',
       controller: function AppCtrl ($scope) {
         $scope.update = function(d, i){ $scope.data = randomData(); };
         function randomData(){
           return d3.range(~~(Math.random()*50)+1).map(function(d, i){return ~~(Math.random()*1000);});
         }
-      },
-      templateUrl: 'partial/chartForm.html'
+      }
     };
   });
 
